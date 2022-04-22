031016d9fa386543c20f0c5fa2f77952a3494a64
Siddhi-Jadhav <siddhi.jadhav@emtecinc.com>
2022-04-22 18:46:47 +0530

    committed on 22/4/2022

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BookRepository } from '../book/book.repository';
-import { UserRepository } from '../user/user.repository';
 import { EmailService } from '../email/email.service';
 import { ReminderModule } from 'src/reminder/reminder.module';
 
