696391da87182ca0390adc542f3dc23ceeb052f2
Siddhi-Jadhav <siddhi.jadhav@emtecinc.com>
2022-04-22 18:43:44 +0530

    committed on 22/4/2022

--- src/reminder/reminder.service.ts
+++ src/reminder/reminder.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { BookUserRepository } from '../BookUserBook/bookuser.repository';
 import { getCustomRepository } from 'typeorm';
-import * as nodemailer from 'nodemailer';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from '../user/user.repository';
 import { BookRepository } from '../book/book.repository';
 import { BookEntity } from '../book/book.entity';
 import { UserEntity } from '../user/user.entity';
+import { EmailService } from 'src/email/email.service';
 interface EmailData {
   book: BookEntity;
   user: UserEntity;
@@ -20,9 +20,11 @@ export class ReminderService {
 
     @InjectRepository(BookRepository)
     private bookRepository: BookRepository,
+
+    private emailService: EmailService,
   ) {}
 
-  @Cron('0 10 * * *')
+  @Cron('* * * * *')
   async run() {
     const bookUserRepository = getCustomRepository(BookUserRepository);
     const records = await bookUserRepository.mailReceivers();
@@ -31,7 +33,6 @@ export class ReminderService {
       const user = await this.userRepository.findOne({
         where: { userId: record.userId },
       });
-      //console.log(user);
       const book = await this.bookRepository.findOne({
         where: { bookId: record.bookId },
       });
@@ -44,34 +45,7 @@ export class ReminderService {
       `there are ${data.length} users who will receive mail for returning the book`,
     );
     await data.map(async (record) => {
-      await this.sendEmail(record);
+      await this.emailService.sendEmail(record);
     });
-  }
-
-  async sendEmail(data: Promise<EmailData>) {
-    const record = await data;
-    //console.log(record);
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false,
-      auth: {
-        user: 'dudley.treutel56@ethereal.email',
-        pass: 'fasqUbrxpY1n7EkqD4',
-      },
-    });
-    try {
-      const info = await transporter.sendMail({
-        from: 'dudley.treutel56@ethereal.email',
-        to: record.user.email,
-        subject: 'Reminder for returning the book',
-        text: 'Hello Reader',
-        html: `<p>Hello ${record.user.username}, you have following book to be returned </p>
-        <b>book - ${record.book.title}</b>`,
-      });
-      console.log('Message sent: %s', info.messageId);
-    } catch (err) {
-      console.log(err.message);
-    }
   }
 }
